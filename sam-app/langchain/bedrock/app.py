245a7c2751a9d6399d1b22643164a9ed76f896b1
DiscreteTom <discrete_tom@outlook.com>
2024-01-03 11:40:46 +0800

    refactor: add CharCommonLayer

--- sam-app/langchain/bedrock/app.py
+++ sam-app/langchain/bedrock/app.py
@@ -8,55 +8,44 @@ from callback import APIGatewayWebSocketCallbackHandler
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 
-
+# environment variables
+model_id = os.environ.get("ModelId", "anthropic.claude-v2:1")
 session_table_name = os.environ["SessionTableName"]
-
-session = boto3.session.Session()
-
-claude_prompt = PromptTemplate.from_template(
-    """
-{history}
-
-Human: {input}
-
-Assistant:
-"""
+ai_prefix = os.environ.get(
+    "AI_Prefix",
+    # by default (Claude), this is "Assistant"
+    "Assistant",
 )
+prompt_template = os.environ.get(
+    "PromptTemplate",
+    # by default (Claude)
+    "\\n{history}\\n\\nHuman: {input}\\n\\nAssistant:\\n",
+).replace("\\n", "\n")
+
+# init clients outside of handler
+llm = Bedrock(model_id=model_id, streaming=True)
+prompt = PromptTemplate.from_template(prompt_template)
+session = boto3.session.Session()
 
 
 def handler(event, context):
     print(json.dumps(event))
-    domain = event["requestContext"]["domainName"]
-    stage = event["requestContext"]["stage"]
     connection_id = event["requestContext"]["connectionId"]
     body = json.loads(event["body"])
 
-    apigw = session.client(
-        "apigatewaymanagementapi",
-        endpoint_url=f"https://{domain}/{stage}",
-    )
-
-    llm = Bedrock(
-        model_id="anthropic.claude-v2:1",
-        streaming=True,
-        callbacks=[APIGatewayWebSocketCallbackHandler(apigw, connection_id)],
-    )
+    # set callback handler
+    # so that every time the model generates a response,
+    # it is sent to the client
+    llm.callbacks = [APIGatewayWebSocketCallbackHandler(session, event)]
 
     history = DynamoDBChatMessageHistory(
         table_name=session_table_name,
         session_id=connection_id,
         boto3_session=session,
     )
-    memory = ConversationBufferMemory(
-        ai_prefix="Assistant",
-        chat_memory=history,
-    )
-
-    conversation = ConversationChain(
-        llm=llm,
-        memory=memory,
-    )
-    conversation.prompt = claude_prompt
+    memory = ConversationBufferMemory(ai_prefix=ai_prefix, chat_memory=history)
+    conversation = ConversationChain(llm=llm, memory=memory)
+    conversation.prompt = prompt
 
     conversation.predict(input=body["input"])
 
