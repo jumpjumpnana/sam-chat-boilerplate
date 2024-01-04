676e9f8bba5c6d78d0b1193d83c9420a54e29c74
DiscreteTom <discrete_tom@outlook.com>
2024-01-04 11:30:25 +0800

    refactor(langchain/openai): rewrite

--- sam-app/langchain/openai/app.py
+++ sam-app/langchain/openai/app.py
@@ -6,46 +6,31 @@ from langchain.llms.openai import OpenAI
 from langchain.chains import ConversationChain
 from callback import APIGatewayWebSocketCallbackHandler
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
-from typing import Dict
 
 session_table_name = os.environ["SessionTableName"]
 open_ai_api_base = os.environ["OpenAI_API_Base"]
 
+llm = OpenAI(openai_api_base=open_ai_api_base, openai_api_key="test", streaming=True)
 session = boto3.session.Session()
 
 
 def handler(event, context):
     print(json.dumps(event))
-    domain = event["requestContext"]["domainName"]
-    stage = event["requestContext"]["stage"]
     connection_id = event["requestContext"]["connectionId"]
     body = json.loads(event["body"])
 
-    apigw = session.client(
-        "apigatewaymanagementapi",
-        endpoint_url=f"https://{domain}/{stage}",
-    )
-
-    llm = OpenAI(
-        openai_api_base=open_ai_api_base,
-        streaming=True,
-        callbacks=[APIGatewayWebSocketCallbackHandler(apigw, connection_id)],
-    )
+    # set callback handler
+    # so that every time the model generates a response,
+    # it is sent to the client
+    llm.callbacks = [APIGatewayWebSocketCallbackHandler(session, event)]
 
     history = DynamoDBChatMessageHistory(
         table_name=session_table_name,
         session_id=connection_id,
         boto3_session=session,
     )
-    memory = ConversationBufferMemory(
-        chat_memory=history,
-    )
-
-    conversation = ConversationChain(
-        llm=llm,
-        memory=memory,
-    )
+    memory = ConversationBufferMemory(chat_memory=history)
+    conversation = ConversationChain(llm=llm, memory=memory)
 
     conversation.predict(input=body["input"])
 
