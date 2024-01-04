fa4cac3b04884fadb33eb000dc7ff2b2223d7476
DiscreteTom <discrete_tom@outlook.com>
2024-01-04 11:36:03 +0800

    refactor(langchain/sagemaker): rewrite

--- sam-app/langchain/sagemaker/app.py
+++ sam-app/langchain/sagemaker/app.py
@@ -6,14 +6,10 @@ from langchain.llms.sagemaker_endpoint import SagemakerEndpoint, LLMContentHandl
 from langchain.chains import ConversationChain
 from callback import APIGatewayWebSocketCallbackHandler
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
 from typing import Dict
 
 session_table_name = os.environ["SessionTableName"]
 endpoint_name = os.environ["EndpointName"]
-
-session = boto3.session.Session()
-sagemaker = session.client("sagemaker-runtime")
 
 
 class ContentHandler(LLMContentHandler):
@@ -29,43 +25,35 @@ class ContentHandler(LLMContentHandler):
         return response_json[0]["generated_text"]
 
 
-content_handler = ContentHandler()
+session = boto3.session.Session()
+sagemaker = session.client("sagemaker-runtime")
+llm = SagemakerEndpoint(
+    endpoint_name=endpoint_name,
+    client=sagemaker,
+    model_kwargs={"temperature": 1e-10},
+    content_handler=ContentHandler(),
+    streaming=True,
+)
 
 
+# TODO: move the common handler to layer
 def handler(event, context):
     print(json.dumps(event))
-    domain = event["requestContext"]["domainName"]
-    stage = event["requestContext"]["stage"]
     connection_id = event["requestContext"]["connectionId"]
     body = json.loads(event["body"])
 
-    apigw = session.client(
-        "apigatewaymanagementapi",
-        endpoint_url=f"https://{domain}/{stage}",
-    )
-
-    llm = SagemakerEndpoint(
-        endpoint_name=endpoint_name,
-        client=sagemaker,
-        model_kwargs={"temperature": 1e-10},
-        content_handler=content_handler,
-        streaming=True,
-        callbacks=[APIGatewayWebSocketCallbackHandler(apigw, connection_id)],
-    )
+    # set callback handler
+    # so that every time the model generates a response,
+    # it is sent to the client
+    llm.callbacks = [APIGatewayWebSocketCallbackHandler(session, event)]
 
     history = DynamoDBChatMessageHistory(
         table_name=session_table_name,
         session_id=connection_id,
         boto3_session=session,
     )
-    memory = ConversationBufferMemory(
-        chat_memory=history,
-    )
-
-    conversation = ConversationChain(
-        llm=llm,
-        memory=memory,
-    )
+    memory = ConversationBufferMemory(chat_memory=history)
+    conversation = ConversationChain(llm=llm, memory=memory)
 
     conversation.predict(input=body["input"])
 
